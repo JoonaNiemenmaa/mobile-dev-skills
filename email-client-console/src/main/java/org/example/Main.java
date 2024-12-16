7d1650227243a1dead3efd3eb716e5123162db41
Joona Niemenmaa <joonaoskari@hotmail.com>
2024-12-16 16:36:55 +0200

    - Refactored the code to add better exception handling

--- email-client-console/src/main/java/org/example/Main.java
+++ email-client-console/src/main/java/org/example/Main.java
@@ -5,16 +5,20 @@ import java.util.ArrayList;
 public class Main {
     public static void main(String[] args) {
         ArrayList<String> recipients = new ArrayList<>();
-        recipients.add("joona@localhost");
+        recipients.add("tomi@localhost");
         Sender sender = Sender.getInstance();
-        sender.sendMail(new Email("joona@localhost", recipients, "Test email", "Moi!\r\nJoona on tosi siisti kooderi :)\r\nmoikka\r\n"));
-        ArrayList<String> verify_list = new ArrayList<>();
+        try {
+            sender.sendMail(new Email("joona@localhost", recipients, "Test email", "Moi!\r\nJoona on tosi siisti kooderi :)\r\nmoikka\r\n"));
+        } catch (SMTPProtocolException exception) {
+            System.err.print("Error: " + exception.getReply());
+        }
+/*        ArrayList<String> verify_list = new ArrayList<>();
         verify_list.add("joona");
         verify_list.add("enoleolemassa");
         verify_list.add("joona@localhost");
         int[] results = sender.verifyUser(verify_list);
         for (int result : results) {
             System.out.println(result);
-        }
+        }*/
     }
 }
--- email-client-console/src/main/java/org/example/Sender.java
+++ email-client-console/src/main/java/org/example/Sender.java
@@ -1,11 +1,8 @@
 package org.example;
 
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.IOException;
+import java.io.*;
 import java.net.Socket;
 import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
 
 public class Sender {
     final private int SMTP_PORT = 25;
@@ -18,18 +15,18 @@ public class Sender {
 
     final private int MINUTE = 1000 * 60; // Minute in milliseconds
 
-    private int INITIAL_TIMEOUT = MINUTE * 5; // Timeout for the initial 220 greeting message from the server
-    private int EHLO_HELO_TIMEOUT = MINUTE * 5;
-    private int MAIL_FROM_TIMEOUT = MINUTE * 5;
-    private int RCPT_TO_TIMEOUT = MINUTE * 5;
-    private int DATA_INITIATE_TIMEOUT = MINUTE * 2;
-    private int DATA_BLOCK_TIMEOUT = MINUTE * 3;    // Java's socket API does not support changing timeouts for write operations thus this field is unfortunately unused for now
-    private int DATA_TERMINATE_TIMEOUT = MINUTE * 10;
+    public int INITIAL_TIMEOUT = MINUTE * 5; // Timeout for the initial 220 greeting message from the server
+    public int EHLO_HELO_TIMEOUT = MINUTE * 5;
+    public int MAIL_FROM_TIMEOUT = MINUTE * 5;
+    public int RCPT_TO_TIMEOUT = MINUTE * 5;
+    public int DATA_INITIATE_TIMEOUT = MINUTE * 2;
+    public int DATA_BLOCK_TIMEOUT = MINUTE * 3;    // Java's socket API does not support changing timeouts for write operations thus this field is unfortunately unused
+    public int DATA_TERMINATE_TIMEOUT = MINUTE * 10;
 
-    private int VRFY_TIMEOUT = MINUTE * 5;  // Timeouts for the VRFY command were not provided in the RFC
+    public int VRFY_TIMEOUT = MINUTE * 5;  // Timeouts for the VRFY command were not provided in the RFC
                                             // So I just placed it at five minutes to match the other timeouts
 
-    private int QUIT_TIMEOUT = 5000; // Again, no provided value in RFC. Since were closing the connection anyway might as well not have a lengthy timeout
+    public int QUIT_TIMEOUT = 5000; // Again, no provided value in RFC. Since were closing the connection anyway might as well not have a lengthy timeout
 
     private Sender() {}
 
@@ -40,177 +37,93 @@ public class Sender {
         return sender;
     }
 
-    private int writeMessage(String message, int timeout, Socket socket) throws IOException {
-        DataOutputStream socket_out = new DataOutputStream(socket.getOutputStream());
-        DataInputStream socket_in = new DataInputStream(socket.getInputStream());
-        System.out.println("C: " + message);
-        message = message + "\r\n"; // Each command sent to the server has to end with <CR><LF> for the server to take action
-        socket_out.write(message.getBytes(StandardCharsets.US_ASCII)); // All messages written to the server have to be in US 7-bit ASCII
-        return readLine(timeout, socket);
-    }
+    public void sendMail(Email email) throws SMTPProtocolException {
+        final String crlf = "\r\n";
+        try (Socket socket = new Socket(ADDRESS, SMTP_PORT)) { // Address and port are hardcoded since the app is only meant to be used with a local mail server
+            PrintWriter writer = new PrintWriter(new OutputStreamWriter(socket.getOutputStream(), StandardCharsets.US_ASCII), true); // SMTP requires that the commands sent using it are in US-ASCII
+            BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 
-    private int readLine(int timeout, Socket socket) throws IOException {
-        DataInputStream socket_in = new DataInputStream(socket.getInputStream());
-        String response = "";
-        socket.setSoTimeout(timeout);
-        char c = (char)socket_in.read();
-        while (socket_in.available() != 0) {
-            response = response + c;
-            c = (char)socket_in.read();
-        }
-        response = response + c;
-        System.out.print("S: " + response);
-        int reply = 0;
-        if (response.length() > 3) {
-            reply = Integer.parseInt(response.substring(0, 3));
-        }
-        return reply; // Only returns the response code
-    }
-
-    public int sendMail(Email email) {
-        Socket socket = null;
-        int reply = 0;
-        try {
-            socket = new Socket(ADDRESS, SMTP_PORT);
-
-            String HOST_ADDRESS = socket.getLocalAddress().getHostAddress();
-
-            System.out.println("--- NEW CONNECTION ---");
-
-            DataOutputStream socket_out = new DataOutputStream(socket.getOutputStream());
-            DataInputStream socket_in = new DataInputStream(socket.getInputStream());
-
-            reply = readLine(INITIAL_TIMEOUT, socket);
-            if (reply == 220) { // Initial reply 220 means that the server has accepted the connection and is ready
-                reply = writeMessage("EHLO " + HOST_ADDRESS, EHLO_HELO_TIMEOUT, socket);
-                if (reply != 250) { // Reply 250 generally indicates success
-                    reply = writeMessage("HELO " + HOST_ADDRESS, EHLO_HELO_TIMEOUT, socket);
-                }
-                if (reply == 250) {
-                    reply = writeMessage("MAIL FROM:<" + email.getSender() + ">", MAIL_FROM_TIMEOUT, socket);
-                }
-                if (reply == 250) {
-                    for (String recipient : email.getRecipients()) {
-                        reply = writeMessage("RCPT TO:<" + recipient + ">", RCPT_TO_TIMEOUT, socket);
-                        if (reply != 250) break;
-                    }
-                }
-                if (reply == 250) {
-                    reply = writeMessage("DATA", DATA_INITIATE_TIMEOUT, socket);
-                    if (reply == 354) {
-                        System.out.print(email.getContent());
-                        socket_out.write(email.getContent().getBytes(StandardCharsets.US_ASCII));
-                        reply = writeMessage(".", DATA_TERMINATE_TIMEOUT, socket);
-                    }
-                }
+            socket.setSoTimeout(INITIAL_TIMEOUT);
+            String reply = reader.readLine();
+            System.out.println("S: " + reply);
+            if (!reply.startsWith("220")) {
+                throw new SMTPProtocolException(reply);
             }
-            writeMessage("QUIT", QUIT_TIMEOUT, socket);
-            socket.close();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-        return reply; // Returns the last return code (Excluding the QUIT command) for error reporting
-    }
 
-    public int verifyUser(String user) {
-        int reply = 0;
-        Socket socket = null;
-        try {
-            socket = new Socket(ADDRESS, SMTP_PORT);
+            String message = "EHLO " + socket.getLocalAddress().getHostAddress() + crlf;
+            writer.printf(message);
+            System.out.print("C: " + message);
 
-            String HOST_ADDRESS = socket.getLocalAddress().getHostAddress();
-
-            System.out.println("--- NEW CONNECTION ---");
-
-            reply = readLine(INITIAL_TIMEOUT, socket);
-
-            if (reply == 220) {
-                reply = writeMessage("EHLO " + HOST_ADDRESS, EHLO_HELO_TIMEOUT, socket);
-                if (reply != 250) {
-                    reply = writeMessage("HELO " + HOST_ADDRESS, EHLO_HELO_TIMEOUT, socket);
+            socket.setSoTimeout(EHLO_HELO_TIMEOUT);
+            reply = reader.readLine();
+            System.out.println("S: " + reply);
+            if (reply.startsWith("250")) {
+                while (reply.startsWith("250-")) { // In multiline replies every line starts with the code and a dash except the last one which starts with the code and a space character
+                    reply = reader.readLine();
+                    System.out.println("S: " + reply);
                 }
-                if (reply == 250) {
-                    reply = writeMessage("VRFY " + user, VRFY_TIMEOUT, socket);
+            } else { // Fallback to HELO if EHLO isn't supported for the initial handshake
+                message = "HELO " + socket.getLocalAddress().getHostAddress() + crlf;
+                writer.printf(message);
+                System.out.print("C: " + message);
+                reply = reader.readLine();
+                if (!reply.startsWith("250")) {
+                    throw new SMTPProtocolException(reply);
                 }
             }
 
-            writeMessage("QUIT", QUIT_TIMEOUT, socket);
-            socket.close();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-        return reply;
-    }
+            message = "MAIL FROM:<" + email.getSender() + ">" + crlf;
+            writer.printf(message);
+            System.out.print("C: " + message);
+            socket.setSoTimeout(MAIL_FROM_TIMEOUT);
+            reply = reader.readLine();
+            System.out.println("S: " + reply);
+            if (!reply.startsWith("250")) {
+                throw new SMTPProtocolException(reply);
+            }
 
-    public int[] verifyUser(ArrayList<String> users) {
-        int reply = 0;
-        int[] result = new int[users.size()];
-        Socket socket = null;
-        try {
-            socket = new Socket(ADDRESS, SMTP_PORT);
-
-            String HOST_ADDRESS = socket.getLocalAddress().getHostAddress();
-
-            System.out.println("--- NEW CONNECTION ---");
-
-            reply = readLine(INITIAL_TIMEOUT, socket);
-
-            if (reply == 220) {
-                reply = writeMessage("EHLO " + HOST_ADDRESS, EHLO_HELO_TIMEOUT, socket);
-                if (reply != 250) {
-                    reply = writeMessage("HELO " + HOST_ADDRESS, EHLO_HELO_TIMEOUT, socket);
-                }
-                if (reply == 250) {
-                    for (int i = 0; i < users.size(); i++) {
-                        reply = writeMessage("VRFY " + users.get(i), VRFY_TIMEOUT, socket);
-                        result[i] = reply;
-                    }
+            for (String recipient : email.getRecipients()) {
+                message = "RCPT TO:<" + recipient + ">" + crlf;
+                writer.printf(message);
+                System.out.print("C: " + message);
+                socket.setSoTimeout(RCPT_TO_TIMEOUT);
+                reply = reader.readLine();
+                System.out.println("S: " + reply);
+                if (!reply.startsWith("250")) {
+                    throw new SMTPProtocolException(reply);
                 }
             }
 
-            writeMessage("QUIT", QUIT_TIMEOUT, socket);
-            socket.close();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+            message = "DATA" + crlf;
+            writer.printf(message);
+            System.out.print("C: " + message);
+            socket.setSoTimeout(DATA_INITIATE_TIMEOUT);
+            reply = reader.readLine();
+            System.out.println("S: " + reply);
+            if (!reply.startsWith("354")) {
+                throw new SMTPProtocolException(reply);
+            }
+
+            writer.printf(email.getContent());
+            System.out.print(email.getContent());
+
+            writer.printf("." + crlf);
+            System.out.println(".");
+            socket.setSoTimeout(DATA_TERMINATE_TIMEOUT);
+            reply = reader.readLine();
+            System.out.println("S: " + reply);
+            if (!reply.startsWith("250")) {
+                throw new SMTPProtocolException(reply);
+            }
+
+            message = "QUIT" + crlf;
+            writer.printf(message);
+            System.out.print("C: " + message);
+            socket.setSoTimeout(QUIT_TIMEOUT);
+            reply = reader.readLine();
+            System.out.println("S: " + reply);
+        } catch (IOException exception) {
+            exception.printStackTrace();
         }
-        return result;
-    }
-
-    // The RFC states that the timeouts for commands should be easily configurable, hence the setters
-
-    public void setInitialTimeout(int INITIAL_TIMEOUT) {
-        this.INITIAL_TIMEOUT = INITIAL_TIMEOUT;
-    }
-
-    public void setGreetingTimeout(int EHLO_HELO_TIMEOUT) {
-        this.EHLO_HELO_TIMEOUT = EHLO_HELO_TIMEOUT;
-    }
-
-    public void setMailFromTimeout(int MAIL_FROM_TIMEOUT) {
-        this.MAIL_FROM_TIMEOUT = MAIL_FROM_TIMEOUT;
-    }
-
-    public void setRcptToTimeout(int RCPT_TO_TIMEOUT) {
-        this.RCPT_TO_TIMEOUT = RCPT_TO_TIMEOUT;
-    }
-
-    public void setDataInitiateTimeout(int DATA_INITIATE_TIMEOUT) {
-        this.DATA_INITIATE_TIMEOUT = DATA_INITIATE_TIMEOUT;
-    }
-
-    public void setDataBlockTimeout(int DATA_BLOCK_TIMEOUT) {
-        this.DATA_BLOCK_TIMEOUT = DATA_BLOCK_TIMEOUT;
-    }
-
-    public void setDataTerminateTimeout(int DATA_TERMINATE_TIMEOUT) {
-        this.DATA_TERMINATE_TIMEOUT = DATA_TERMINATE_TIMEOUT;
-    }
-
-    public void setVrfyTimeout(int VRFY_TIMEOUT) {
-        this.VRFY_TIMEOUT = VRFY_TIMEOUT;
-    }
-
-    public void setQUIT_TIMEOUT(int QUIT_TIMEOUT) {
-        this.QUIT_TIMEOUT = QUIT_TIMEOUT;
     }
 }
