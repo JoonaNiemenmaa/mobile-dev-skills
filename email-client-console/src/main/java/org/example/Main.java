e7fd84b6f3ef6f07bf0bd182318f9ba21a879ac3
Joona Niemenmaa <joonaoskari@hotmail.com>
2024-12-29 13:23:21 +0200

    - Finished up implementing the receiver class

--- email-client-console/src/main/java/org/example/Main.java
+++ email-client-console/src/main/java/org/example/Main.java
@@ -1,11 +1,22 @@
 package org.example;
 
 import java.util.ArrayList;
-import java.util.Base64;
 
 public class Main {
     public static void main(String[] args) {
-        Receiver receiver = new Receiver();
-        receiver.startSession("joona", "lupu1tupu");
+        Sender sender = Sender.getInstance();
+        Receiver receiver = new Receiver("test1", "pwd1");
+        String[] recipients = new String[]{
+                "test1@localhost"
+        };
+        Email email = new Email("test1@localhost", recipients, "Hello!", "Hello!\r\nI am very cool\r\n");
+        try {
+            sender.sendMail(email);
+        } catch (SMTPProtocolException e) {
+            throw new RuntimeException(e);
+        }
+        receiver.start();
+        receiver.addAction("body FETCH 1 BODY[TEXT]");
+
     }
 }
--- email-client-console/src/main/java/org/example/Receiver.java
+++ email-client-console/src/main/java/org/example/Receiver.java
@@ -5,17 +5,36 @@ import java.net.Socket;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Base64;
+import java.util.HashMap;
 
-public class Receiver {
+public class Receiver extends Thread {
 
     final private String ADDRESS = "localhost"; // Again, address is hardcoded since the program is only meant to work with a local mail server
-    final private int IMAP_PORT = 143;
+    final private int IMAP_PORT = 3143;
 
-    private ArrayList<String> action_queue = new ArrayList<>();
+    final private String name;
+    final private String password;
 
-    public Receiver() {}
+    final private ArrayList<String> action_queue = new ArrayList<>();
+    final private ArrayList<String> mailboxes = new ArrayList<>();
+    final private HashMap<String, String> replies = new HashMap<>();
 
-    public void startSession(String name, String password) throws IMAPProtocolException {
+    public Receiver(String name, String password) {
+        super();
+        this.name = name;
+        this.password = password;
+    }
+
+    public void addAction(String action) {
+        action_queue.add(action);
+    }
+
+    public String getReply(String code) {
+        return replies.remove(code);
+    }
+
+    @Override
+    public void run() throws IMAPProtocolException {
         final String crlf = "\r\n";
         try (Socket socket = new Socket(ADDRESS, IMAP_PORT)) {
             PrintWriter writer = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()), true);
@@ -26,7 +45,7 @@ public class Receiver {
             String response = reader.readLine(); // Read the initial greeting from the server
             System.out.println("S: " + response);
 
-            String message = message_code++ + " CAPABILITY" + crlf;
+            String message = message_code + " CAPABILITY" + crlf;
             writer.printf(message);
             System.out.print("C: " + message);
             response = reader.readLine(); // first response to CAPABILITY includes a list of all supported features of the server unless an error occurs
@@ -44,7 +63,7 @@ public class Receiver {
             System.out.println("S: " + response);
 
             if (capabilities.contains("AUTH=PLAIN")) {
-                message = message_code++ + " AUTHENTICATE PLAIN" + crlf;
+                message = ++message_code + " AUTHENTICATE PLAIN" + crlf;
                 writer.printf(message);
                 System.out.print("C: " + message);
                 response = reader.readLine();
@@ -63,7 +82,7 @@ public class Receiver {
                 response = reader.readLine();
                 System.out.println("S: " + response);
             } else {
-                message = message_code++ + " LOGIN " + name + " " + password + crlf;
+                message = ++message_code + " LOGIN " + name + " " + password + crlf;
                 writer.printf(message);
                 System.out.print("C: " + message);
                 response = reader.readLine();
@@ -71,15 +90,85 @@ public class Receiver {
                 if (response.startsWith(message_code + " NO") || response.startsWith(message_code + " BAD")) {
                     throw new IMAPProtocolException(response);
                 }
+            }
+
+            if (response.startsWith(message_code + " OK")) {
+
+                /*message = ++message_code + " NAMESPACE" + crlf; // Namespace command not supported in IMAP4rev1
+                writer.printf(message);
+                System.out.print("C: " + message);
                 response = reader.readLine();
                 System.out.println("S: " + response);
+                if (response.startsWith(message_code + " NO") || response.startsWith(message_code + " BAD")) {
+                    throw new IMAPProtocolException(response);
+                }
+                String[] namespace = response.split("\"");
+                String prefix = namespace[1];
+                String delimiter = namespace[3];
+                response = reader.readLine();
+                System.out.println("S: " + response);*/
+
+                message = ++message_code + " LIST \"\" \"*\""  + crlf;
+                writer.printf(message);
+                System.out.print("C: " + message);
+                response = reader.readLine();
+                System.out.println("S: " + response);
+                if (response.startsWith(message_code + " NO") || response.startsWith(message_code + " BAD")) {
+                    throw new IMAPProtocolException(response);
+                }
+                if (response.startsWith("*")) {
+                    String[] split = response.split(" ");
+                    mailboxes.add(split[split.length - 1]);
+                }
+                while (response.startsWith("*")) {
+                    response = reader.readLine();
+                    if (response.startsWith("*")) {
+                        String[] split = response.split(" ");
+                        mailboxes.add(split[split.length - 1]);
+                    }
+                    System.out.println("S: " + response);
+                }
+                System.out.println(mailboxes);
+
+                message = ++message_code + " SELECT INBOX"  + crlf; // An IMAP server implicitly always has a mailbox called INBOX
+                writer.printf(message);
+                System.out.print("C: " + message);
+                response = reader.readLine();
+                System.out.println("S: " + response);
+                if (response.startsWith(message_code + " NO") || response.startsWith(message_code + " BAD")) {
+                    throw new IMAPProtocolException(response);
+                }
+                while (response.startsWith("*")) {
+                    response = reader.readLine();
+                    System.out.println("S: " + response);
+                }
+
+                while (true) {
+                    while (action_queue.isEmpty()) {
+                        Thread.sleep(100);
+                    }
+                    String command = action_queue.removeFirst();
+                    String code = command.split(" ")[0];
+
+                    if (command.split(" ")[1].equals("LOGOUT")) break;
+
+                    writer.printf(command + crlf);
+                    System.out.println("C: " + command);
+
+                    String reply = "";
+                    response = reader.readLine();
+                    System.out.println("S: " + response);
+                    while (!response.startsWith(code)) {
+                        reply = reply + response + crlf;
+                        response = reader.readLine();
+                        System.out.println("S: " + response);
+                    }
+
+                    replies.put(code, reply);
+                }
             }
 
-            if (response.startsWith((message_code - 1) + " OK")) { // Need to subtract 1 from message code to match it with the authentication command code
-                // Start the session here
-            }
-
-            message = message_code + " LOGOUT" + crlf;
+            message = ++message_code + " LOGOUT" + crlf;
             writer.printf(message);
             System.out.print("C: " + message);
             response = reader.readLine();
@@ -87,6 +176,8 @@ public class Receiver {
             response = reader.readLine();
             System.out.println("S: " + response);
 
+        } catch (InterruptedException exception) {
+            exception.printStackTrace();
         } catch (IOException exception) {
             exception.printStackTrace();
         }
